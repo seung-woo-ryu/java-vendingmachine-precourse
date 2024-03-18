942e85dee729a375d854709ae2a38d16f07b591d
seung-woo-ryu <tmddn645@naver.com>
2024-03-18 16:44:35 +0900

    feat: money2Coin getter 추가, coin2int List 정적 메서드 추가

--- src/main/java/vendingmachine/Coin.java
+++ src/main/java/vendingmachine/Coin.java
@@ -1,5 +1,9 @@
 package vendingmachine;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Coin {
     COIN_500(500),
     COIN_100(100),
@@ -15,4 +19,18 @@ public enum Coin {
     public int getAmount() {
         return amount;
     }
+
+    public static Coin getCoin(int money){
+        for (Coin c : Coin.values()) {
+            if (c.getAmount() == money) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException("[ERROR] 잘못된 동전 금액입니다.");
+    }
+
+    public static List<Integer> getCoinList() {
+        return Arrays.stream(Coin.values()).map(c -> c.getAmount()).collect(Collectors.toList());
+    }
+
 }
