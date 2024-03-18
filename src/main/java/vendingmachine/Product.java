36ca2ee1188fcd05b337ec2167102bf5b51a1f2a
seung-woo-ryu <tmddn645@naver.com>
2024-03-18 16:51:32 +0900

    feat: product 클래스 생성, 유효성 검사 추가

---
+++ src/main/java/vendingmachine/Product.java
@@ -0,0 +1,27 @@
+package vendingmachine;
+
+public class Product {
+
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Product(String name, int price, int quantity) {
+        // 유효성 검사
+        checkMoney(price);
+
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    private void checkMoney(int price) {
+        if (price < 100) {
+            throw new IllegalArgumentException("[ERROR] 상품 가격은 100원 이상입니다.");
+        }
+
+        if (price % 10 != 0) {
+            throw new IllegalArgumentException("[ERROR] 상품 가격은 10원으로 나누어 떨어져야 합니다.");
+        }
+    }
+}
