20024d30c83d32114841c46385a6d59f76ebb707
seung-woo-ryu <tmddn645@naver.com>
2024-03-19 16:12:06 +0900

    feat: add Controller class

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -2,18 +2,7 @@ package vendingmachine;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        View view = new View();
-
-        // ’자판기’ 보유하고 있는 금액 입력.
-        int inputMoney = view.inputMoney();
-
-        // 금액에 맞는 ‘동전’ 랜덤 생성.
-        // ‘자판기’ 보유한 ‘동전’ 출력.
-        // “상품명”, “가격”, “수량” 입력 받아 ‘물품’ 생성
-        // ‘투입 금액’ 입력
-        // 구매할 “상품명” 입력
-        // 5,6 반복 후 잔돈 반환
-
+        Controller controller = new Controller();
+        controller.run();
     }
 }
---
+++ src/main/java/vendingmachine/Controller.java
@@ -0,0 +1,41 @@
+package vendingmachine;
+
+import java.util.EnumMap;
+import java.util.List;
+import vendingmachine.view.View;
+
+public class Controller {
+
+    private VendingMachine vendingMachine;
+    private View view;
+
+    public Controller() {
+        init();
+    }
+
+    private void init() {
+        vendingMachine = new VendingMachine();
+        view = new View();
+    }
+
+    public void run() {
+        // ’자판기’ 보유하고 있는 금액 입력.
+        // 금액에 맞는 ‘동전’ 랜덤 생성.
+        vendingMachine.generateCoins(view.inputVendingMachineMoney());
+        // ‘자판기’ 보유한 ‘동전’ 출력.
+        view.printVendingMachineCoins(vendingMachine.getCoinMap());
+        // “상품명”, “가격”, “수량” 입력 받아 ‘물품’ 생성
+        ProductContainer productContainer = ProductContainer.of(view.inputProducts());
+        // ‘투입 금액’ 입력
+        // 상품 구매
+        dealProcess(view.inputClientMoney());
+    }
+
+    private void dealProcess(int clientMoney) {
+        while (true) {
+            // view.inputBuyProduct();
+            break;
+        }
+        view.printChange(clientMoney,vendingMachine.returnCoins(clientMoney));
+    }
+}
